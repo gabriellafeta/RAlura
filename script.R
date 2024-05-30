02efcaa388588fce03c13cae20e313d79e969352
gabriellafeta <gabriellafetnascimento@gmail.com>
2024-05-30 11:01:34 -0300

    commit

---
+++ script.R
@@ -0,0 +1,7 @@
+install.packages('data.table')
+install.packages('dplyr')
+install.packages('ggplot2')
+install.packages('plotly')
+install.packages('shiny')
+install.packages('shinyWidgets')
+install.packages('shinydashboard')
